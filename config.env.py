4538568ee8cea3f604591de204f8ff0921e19195
Joseph Abbate <josephabbateny@gmail.com>
2021-11-10 19:03:28 -0500

    Change env default for new OIDC Creds

--- config.env.py
+++ config.env.py
@@ -3,7 +3,7 @@ from os import environ as env
 # Flask config
 IP = env.get('IP', '0.0.0.0')
 PORT = env.get('PORT', 8080)
-SERVER_NAME = env.get('SERVER_NAME', 'mylight.csh.rit.edu')
+SERVER_NAME = env.get('SERVER_NAME', 'light.csh.rit.edu')
 
 # DB Info
 SQLALCHEMY_DATABASE_URI = env.get('SQLALCHEMY_DATABASE_URI')
@@ -14,7 +14,7 @@ SECRET_KEY = env.get("SECRET_KEY", default='SECRET-KEY')
 
 # OpenID Connect SSO config CSH
 OIDC_ISSUER = env.get('OIDC_ISSUER', 'https://sso.csh.rit.edu/auth/realms/csh')
-OIDC_CLIENT_ID = env.get('OIDC_CLIENT_ID', 'rideboard')
+OIDC_CLIENT_ID = env.get('OIDC_CLIENT_ID', 'light')
 OIDC_CLIENT_SECRET = env.get('OIDC_CLIENT_SECRET', 'NOT-A-SECRET')
 
 LDAP_BIND_DN = env.get("LDAP_BIND_DN", default="cn=rides,ou=Apps,dc=csh,dc=rit,dc=edu")
